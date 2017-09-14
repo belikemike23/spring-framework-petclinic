c369e14b54d996e191f22a54e5bde0e92bbcb4b3
mike.wang <wangwl1111@gmail.com>
2017-09-14 20:16:15 +0800

    first step

--- src/main/java/org/springframework/samples/petclinic/model/Question.java
+++ src/main/java/org/springframework/samples/petclinic/model/Question.java
@@ -1,4 +1,22 @@
 package org.springframework.samples.petclinic.model;
 
-public class Question {
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Simple JavaBean domain object representing an question.
+ *
+ * @author Mike Wang
+ */
+@Entity
+@Table(name = "question")
+public class Question extends BaseEntity{
+
+    @Column(name = "content")
+    @NotEmpty
+    private String address;
+
 }
